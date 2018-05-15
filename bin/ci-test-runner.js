3b2be5da534cb0b8c3fd7a3d31096fe2b6387cd2
Adam Zieliński <adam@adamziel.com>
2018-05-15 16:16:55 +0200

    Change output directory of coverage reports

--- bin/ci-test-runner.js
+++ bin/ci-test-runner.js
@@ -31,7 +31,7 @@ execSync(
 		'--colors',
 		'--coverage',
 		'--coverageDirectory',
-		'/tmp/artifacts',
+		'./coverage',
 		...process.argv
 			.slice( 3 )
 			.filter( path => /\/test\//.test( path ) )
