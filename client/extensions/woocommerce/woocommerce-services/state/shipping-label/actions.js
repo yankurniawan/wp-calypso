6f572bffd2d799f6f16429946882bec283c398c7
Daniel Rey López <daniel.rey.lopez@a8c.com>
2018-05-21 17:15:44 +0100

    Add a dummy Customs form step if the order requires it
    In this PR:
    - Logic to check if an order (origin + destination) needs a Customs form. Since the addresses can be changed inside the Purchase Label modal, that is constantly being recalculated.
    - If the Customs form is required, show a Customs step after the Packages step.
    - If it's not required, don't show it.
    - All the validation logic is already there. To showcase it, I've added a dummy checkbox. Validation will pass if the checkbox is selected. Note that, when opening the modal, the checkbox is not selected, so the step is "invalid", but there's no error visible as to not oversaturate the user with red. When you submit the step, validation is performed and from that point on you can only submit if the step is valid.
    - After submitting the Customs step, the Rates are refreshed.

--- client/extensions/woocommerce/woocommerce-services/state/shipping-label/actions.js
+++ client/extensions/woocommerce/woocommerce-services/state/shipping-label/actions.js
@@ -89,6 +89,7 @@ import {
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_ADD_ITEMS,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_SET_EMAIL_DETAILS,
 	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_SET_FULFILL_ORDER,
+	WOOCOMMERCE_SERVICES_SHIPPING_LABEL_SAVE_CUSTOMS,
 } from '../action-types.js';
 
 export const fetchLabelsData = ( orderId, siteId ) => dispatch => {
@@ -528,6 +529,20 @@ export const removeItem = ( orderId, siteId, packageId, itemIndex ) => ( dispatc
 export const confirmPackages = ( orderId, siteId ) => ( dispatch, getState ) => {
 	dispatch( toggleStep( orderId, siteId, 'packages' ) );
 	dispatch( savePackages( orderId, siteId ) );
+	tryGetLabelRates( orderId, siteId, dispatch, getState );
+};
+
+export const saveCustoms = ( orderId, siteId ) => {
+	return {
+		type: WOOCOMMERCE_SERVICES_SHIPPING_LABEL_SAVE_CUSTOMS,
+		siteId,
+		orderId,
+	};
+};
+
+export const confirmCustoms = ( orderId, siteId ) => ( dispatch, getState ) => {
+	dispatch( toggleStep( orderId, siteId, 'customs' ) );
+	dispatch( saveCustoms( orderId, siteId ) );
 	tryGetLabelRates( orderId, siteId, dispatch, getState );
 };
 
